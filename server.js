5a9862910858c43cad8336c56e86aa4f66afe5e8
Sarah Beecroft <44879145+SarahMBeecroft@users.noreply.github.com>
2021-01-30 18:53:23 -0800

    Added movie search function and dotenv to hide api key

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
--- src/screens/SearchScreen/index.js
+++ src/screens/SearchScreen/index.js
@@ -1,22 +1,42 @@
-import React, {Component} from 'react';
-import {Container, Row, Col} from '../../components/Grid';
+import React, {useState, useEffect} from 'react';
+import MovieList from '../../components/MovieList';
+import MovieHeader from '../../components/MovieHeader';
+import SearchBox from '../../components/SearchBox';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
-class SearchMovies extends Component {
-  // Creates state
-  state = {
-    search: '',
-    movies: [],
-    error: '',
-    message: '',
+const OMDB_API_KEY = process.env.REACT_APP_API_KEY;
+
+const SearchMovies = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=${OMDB_API_KEY}`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
   };
 
-  render() {
-    return (
-      <Container fluid>
-        <h1>Search Movies</h1>
-      </Container>
-    );
-  }
-}
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
+  return (
+    <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieHeader heading="Search for movies:" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
+      <div className="row">
+        <MovieList movies={movies} />
+      </div>
+    </div>
+  );
+};
 
 export default SearchMovies;
