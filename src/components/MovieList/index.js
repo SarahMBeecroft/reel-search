1b9314a49a5d1f75211ac77aa9df74033e3230ac
Sarah Beecroft <44879145+SarahMBeecroft@users.noreply.github.com>
2021-02-01 16:31:23 -0800

    Added favorite button

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import './index.css';
 
 const MovieList = (props) => {
   return (
     <>
       {props.movies.map((movie, index) => (
-        <div className="image-container d-flex justify-content-start m-3">
+        <div className="image-container d-flex row justify-content-start m-3">
           <img src={movie.Poster} alt="movie"></img>
+
           <div className="overlay d-flex align-items-center justify-content-center">
-            Add to favorites
+            <i class="fa fa-heart"></i>&nbsp; Add to favorites
           </div>
         </div>
       ))}
